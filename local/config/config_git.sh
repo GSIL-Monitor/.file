dbafa12476e2e302e626f3328d6039a38404e234
Dong Jian <timeflydj@gmail.com>
2017-08-19 11:00:12 +0800

    add config for git

---
+++ local/config/config_git.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Dong Jian"
+git config --global user.email "timeflydj@gmail.com"
+git config --global push.default simple
+git config --global core.editor emacs
+git config --global core.autocrlf input
